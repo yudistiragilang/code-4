8b0a861a61c365d9bb3bde20e369f0d8cc93f488
Yudhistira Gilang <yudhistiragilang22@gmail.com>
2020-05-15 00:06:56 +0700

    toggle responsive

--- app/Views/maintenance/user.php
+++ app/Views/maintenance/user.php
@@ -25,6 +25,12 @@
     <div class="main-wrapper">
       <div class="navbar-bg"></div>
       <nav class="navbar navbar-expand-lg main-navbar justify-content-end">
+        <div class="form-inline mr-auto">
+          <ul class="navbar-nav navbar-left mr-3">
+            <li><a href="#" data-toggle="sidebar" class="nav-link nav-link-lg"><i class="fas fa-bars"></i></a></li>
+          </ul>
+        </div>
+
         <ul class="navbar-nav navbar-right">
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             <img alt="image" src="<?php echo base_url(); ?>/public/assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
